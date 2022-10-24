f214f2c456a7cf9b17ff44fa04c45bf6e07e130a
Jervx <louellagracechua@gmail.com>
2022-10-24 12:47:22 +0800

    +Splash Screen Activity, +Login Activity

--- app/src/main/java/com/example/libma/MainActivity.java
+++ app/src/main/java/com/example/libma/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.libma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        handler.postDelayed(() -> {
+            Intent loginIntent = new Intent(getApplicationContext(), login.class);
+            System.out.println("Started");
+            startActivity(loginIntent);
+            finish();
+            System.out.println("Finished");
+        }, 2000);
     }
 }
