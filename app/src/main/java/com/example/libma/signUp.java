503b705bb2f82614d01c303d25dd70fcd9dea6b1
johnnel24 <johnnelmagtalas23@gmail.com>
2022-11-02 19:41:10 +0800

    HelloWorld

--- app/src/main/java/com/example/libma/signUp.java
+++ app/src/main/java/com/example/libma/signUp.java
@@ -2,17 +2,18 @@ package com.example.libma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class login extends AppCompatActivity {
+public class signUp  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
-
-
 
     }
 }
+
